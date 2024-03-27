d8fee16ee7ead043b3c3d34222871d9523894394
Philip-21 <philipuzomaobiora@gmail.com>
2024-03-27 13:06:18 +0100

    setting up evmconnect handlers
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- internal/metrics/api.go
+++ internal/metrics/api.go
@@ -3,27 +3,16 @@ package metrics
 import (
 	"context"
 
-	"github.com/gorilla/mux"
-	"github.com/hyperledger/firefly-common/pkg/config"
-	"github.com/hyperledger/firefly-common/pkg/httpserver"
-	"github.com/hyperledger/firefly-evmconnect/internal/evmconfig"
 	"github.com/hyperledger/firefly-transaction-manager/pkg/fftm"
 )
 
 type Manager struct {
 	ctx               context.Context
-	metricsManager    Metrics
-	metricsServer     httpserver.HTTPServer
+	metricsManager    EvmMetrics
 	metricsEnabled    bool
 	metricsHandled    bool
 	metricsServerDone chan error
 	fftm.Manager
-}
-
-func (m *Manager) CreateMetricsMuxRouter() *mux.Router {
-	r := mux.NewRouter()
-	r.Path(config.GetString(evmconfig.MetricsPath)).Handler(m.metricsManager.HTTPHandler())
-	return r
 }
 
 func (m *Manager) MetricsServer() {
@@ -34,12 +23,18 @@ func (m *Manager) MetricsServer() {
 	}
 }
 
-func (m *Manager) initServices(ctx context.Context) (err error) {
-	if m.metricsEnabled {
-		m.metricsServer, err = httpserver.NewHTTPServer(ctx, "metrics", m.CreateMetricsMuxRouter(), m.metricsServerDone, evmconfig.MetricsConfig, evmconfig.CorsConfig)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
+// func (m *Manager) CreateMetricsMuxRouter() *mux.Router {
+// 	r := mux.NewRouter()
+// 	r.Path(config.GetString(evmconfig.MetricsPath)).Handler(m.metricsManager.HTTPHandler())
+// 	return r
+// }
+
+// func (m *Manager) initServices(ctx context.Context) (err error) {
+// 	if m.metricsEnabled {
+// 		m.metricsServer, err = httpserver.NewHTTPServer(ctx, "metrics", m.CreateMetricsMuxRouter(), m.metricsServerDone, evmconfig.MetricsConfig, evmconfig.CorsConfig)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+// 	return nil
+// }
--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -9,7 +9,6 @@ import (
 	"github.com/hyperledger/firefly-common/pkg/config"
 	"github.com/hyperledger/firefly-common/pkg/metric"
 	"github.com/hyperledger/firefly-evmconnect/internal/evmconfig"
-	"github.com/hyperledger/firefly-transaction-manager/pkg/txhandler"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
@@ -17,35 +16,29 @@ import (
 const metricsEvmManagerComponentName = "evmconnect"
 
 // REST api-server and transaction handler are sub-subsystem
-var metricsTransactionHandlerSubsystemName = "th"
+var metricsTransactionHandlerSubsystemName = "evm"
 var metricsRESTAPIServerSubSystemName = "api_server_rest"
 
 var mutex = &sync.Mutex{}
 
-type metricsManager struct {
-	ctx                     context.Context
-	metricsEnabled          bool
-	metricsRegistry         metric.MetricsRegistry
-	txHandlerMetricsManager metric.MetricsManager
-	timeMap                 map[string]time.Time
+type EvmMetricsManager struct {
+	ctx                      context.Context
+	metricsEnabled           bool
+	metricsRegistry          metric.MetricsRegistry
+	EvmHandlerMetricsManager metric.MetricsManager
+	timeMap                  map[string]time.Time
 }
 
-type Metrics interface {
+type EvmMetrics interface {
 	IsMetricsEnabled() bool
 
-	// HTTPHandler returns the HTTP handler of this metrics registry
-	HTTPHandler() http.Handler
-
-	//Get the Api middleware and return a handler instace for the middleware
-	GetAPIServerRESTHTTPMiddleware() func(next http.Handler) http.Handler
-
-	// functions for transaction handler to define and emmit metrics
+	// functions for Evm transaction handlers to define and emmit metrics
 	TransactionHandlerMetrics
 }
 
-func NewMetricsManager(ctx context.Context) Metrics {
+func NewMetricsManager(ctx context.Context) EvmMetrics {
 	metricsRegistry := metric.NewPrometheusMetricsRegistry(metricsEvmManagerComponentName)
-	txHandlerMetricsManager, _ := metricsRegistry.NewMetricsManagerForSubsystem(ctx, metricsRESTAPIServerSubSystemName)
+	EvmHandlerMetricsManager, _ := metricsRegistry.NewMetricsManagerForSubsystem(ctx, metricsRESTAPIServerSubSystemName)
 	_ = metricsRegistry.NewHTTPMetricsInstrumentationsForSubsystem(
 		ctx,
 		metricsRESTAPIServerSubSystemName,
@@ -53,118 +46,113 @@ func NewMetricsManager(ctx context.Context) Metrics {
 		prometheus.DefBuckets,
 		map[string]string{},
 	)
-	mm := &metricsManager{
-		ctx:                     ctx,
-		metricsEnabled:          config.GetBool(evmconfig.MetricsEnabled),
-		timeMap:                 make(map[string]time.Time),
-		metricsRegistry:         metricsRegistry,
-		txHandlerMetricsManager: txHandlerMetricsManager,
+	mm := &EvmMetricsManager{
+		ctx:                      ctx,
+		metricsEnabled:           config.GetBool(evmconfig.MetricsEnabled),
+		timeMap:                  make(map[string]time.Time),
+		metricsRegistry:          metricsRegistry,
+		EvmHandlerMetricsManager: EvmHandlerMetricsManager,
 	}
 	return mm
 }
 
-func (mm *metricsManager) IsMetricsEnabled() bool {
+func (mm *EvmMetricsManager) IsMetricsEnabled() bool {
 	return mm.metricsEnabled
 }
 
-func (mm *metricsManager) HTTPHandler() http.Handler {
+func (mm *EvmMetricsManager) HTTPHandler() http.Handler {
 	httpMiddleware, _ := mm.metricsRegistry.HTTPHandler(mm.ctx, promhttp.HandlerOpts{})
 	return httpMiddleware
 }
 
 // Transaction handler metrics are defined and emitted by transaction handlers
 type TransactionHandlerMetrics interface {
-	txhandler.TransactionMetrics
+	EvmTransactionHandlerMetrics
 }
 
-func (mm *metricsManager) GetAPIServerRESTHTTPMiddleware() func(next http.Handler) http.Handler {
-	httpMiddleware, _ := mm.metricsRegistry.GetHTTPMetricsInstrumentationsMiddlewareForSubsystem(mm.ctx, metricsRESTAPIServerSubSystemName)
-	return httpMiddleware
-}
-
-func (mm *metricsManager) InitTxHandlerCounterMetric(ctx context.Context, metricName string, helpText string, withDefaultLabels bool) {
+func (mm *EvmMetricsManager) InitEvmHandlerCounterMetric(ctx context.Context, metricName string, helpText string, withDefaultLabels bool) {
 	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.NewCounterMetric(ctx, metricName, helpText, withDefaultLabels)
+		mm.EvmHandlerMetricsManager.NewCounterMetric(ctx, metricName, helpText, withDefaultLabels)
 	}
 }
 
-func (mm *metricsManager) InitTxHandlerCounterMetricWithLabels(ctx context.Context, metricName string, helpText string, labelNames []string, withDefaultLabels bool) {
+func (mm *EvmMetricsManager) InitEvmHandlerCounterMetricWithLabels(ctx context.Context, metricName string, helpText string, labelNames []string, withDefaultLabels bool) {
 	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.NewCounterMetricWithLabels(ctx, metricName, helpText, labelNames, withDefaultLabels)
+		mm.EvmHandlerMetricsManager.NewCounterMetricWithLabels(ctx, metricName, helpText, labelNames, withDefaultLabels)
 	}
 }
 
-func (mm *metricsManager) InitTxHandlerGaugeMetric(ctx context.Context, metricName string, helpText string, withDefaultLabels bool) {
+func (mm *EvmMetricsManager) InitEvmHandlerGaugeMetric(ctx context.Context, metricName string, helpText string, withDefaultLabels bool) {
 	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.NewGaugeMetric(ctx, metricName, helpText, withDefaultLabels)
+		mm.EvmHandlerMetricsManager.NewGaugeMetric(ctx, metricName, helpText, withDefaultLabels)
 	}
 }
-func (mm *metricsManager) InitTxHandlerGaugeMetricWithLabels(ctx context.Context, metricName string, helpText string, labelNames []string, withDefaultLabels bool) {
+func (mm *EvmMetricsManager) InitEvmHandlerGaugeMetricWithLabels(ctx context.Context, metricName string, helpText string, labelNames []string, withDefaultLabels bool) {
 	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.NewGaugeMetricWithLabels(ctx, metricName, helpText, labelNames, withDefaultLabels)
+		mm.EvmHandlerMetricsManager.NewGaugeMetricWithLabels(ctx, metricName, helpText, labelNames, withDefaultLabels)
 	}
 }
-func (mm *metricsManager) InitTxHandlerHistogramMetric(ctx context.Context, metricName string, helpText string, buckets []float64, withDefaultLabels bool) {
+func (mm *EvmMetricsManager) InitEvmHandlerHistogramMetric(ctx context.Context, metricName string, helpText string, buckets []float64, withDefaultLabels bool) {
 	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.NewHistogramMetric(ctx, metricName, helpText, buckets, withDefaultLabels)
+		mm.EvmHandlerMetricsManager.NewHistogramMetric(ctx, metricName, helpText, buckets, withDefaultLabels)
 	}
 }
-func (mm *metricsManager) InitTxHandlerHistogramMetricWithLabels(ctx context.Context, metricName string, helpText string, buckets []float64, labelNames []string, withDefaultLabels bool) {
+func (mm *EvmMetricsManager) InitEvmHandlerHistogramMetricWithLabels(ctx context.Context, metricName string, helpText string, buckets []float64, labelNames []string, withDefaultLabels bool) {
 	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.NewHistogramMetricWithLabels(ctx, metricName, helpText, buckets, labelNames, withDefaultLabels)
+		mm.EvmHandlerMetricsManager.NewHistogramMetricWithLabels(ctx, metricName, helpText, buckets, labelNames, withDefaultLabels)
 	}
 }
-func (mm *metricsManager) InitTxHandlerSummaryMetric(ctx context.Context, metricName string, helpText string, withDefaultLabels bool) {
+func (mm *EvmMetricsManager) InitEvmHandlerSummaryMetric(ctx context.Context, metricName string, helpText string, withDefaultLabels bool) {
 	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.NewSummaryMetric(ctx, metricName, helpText, withDefaultLabels)
+		mm.EvmHandlerMetricsManager.NewSummaryMetric(ctx, metricName, helpText, withDefaultLabels)
 	}
 }
-func (mm *metricsManager) InitTxHandlerSummaryMetricWithLabels(ctx context.Context, metricName string, helpText string, labelNames []string, withDefaultLabels bool) {
+func (mm *EvmMetricsManager) InitEvmHandlerSummaryMetricWithLabels(ctx context.Context, metricName string, helpText string, labelNames []string, withDefaultLabels bool) {
 	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.NewSummaryMetricWithLabels(ctx, metricName, helpText, labelNames, withDefaultLabels)
+		mm.EvmHandlerMetricsManager.NewSummaryMetricWithLabels(ctx, metricName, helpText, labelNames, withDefaultLabels)
 	}
 }
 
 // functions for use existing metrics
-func (mm *metricsManager) SetTxHandlerGaugeMetric(ctx context.Context, metricName string, number float64, defaultLabels *metric.FireflyDefaultLabels) {
+func (mm *EvmMetricsManager) SetEvmHandlerGaugeMetric(ctx context.Context, metricName string, number float64, defaultLabels *metric.FireflyDefaultLabels) {
 	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.SetGaugeMetric(ctx, metricName, number, defaultLabels)
+		mm.EvmHandlerMetricsManager.SetGaugeMetric(ctx, metricName, number, defaultLabels)
 	}
 }
-func (mm *metricsManager) SetTxHandlerGaugeMetricWithLabels(ctx context.Context, metricName string, number float64, labels map[string]string, defaultLabels *metric.FireflyDefaultLabels) {
+func (mm *EvmMetricsManager) SetEvmHandlerGaugeMetricWithLabels(ctx context.Context, metricName string, number float64, labels map[string]string, defaultLabels *metric.FireflyDefaultLabels) {
 	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.SetGaugeMetricWithLabels(ctx, metricName, number, labels, defaultLabels)
-	}
-}
-
-func (mm *metricsManager) IncTxHandlerCounterMetric(ctx context.Context, metricName string, defaultLabels *metric.FireflyDefaultLabels) {
-	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.IncCounterMetric(ctx, metricName, defaultLabels)
-	}
-}
-func (mm *metricsManager) IncTxHandlerCounterMetricWithLabels(ctx context.Context, metricName string, labels map[string]string, defaultLabels *metric.FireflyDefaultLabels) {
-	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.IncCounterMetricWithLabels(ctx, metricName, labels, defaultLabels)
-	}
-}
-func (mm *metricsManager) ObserveTxHandlerHistogramMetric(ctx context.Context, metricName string, number float64, defaultLabels *metric.FireflyDefaultLabels) {
-	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.ObserveHistogramMetric(ctx, metricName, number, defaultLabels)
-	}
-}
-func (mm *metricsManager) ObserveTxHandlerHistogramMetricWithLabels(ctx context.Context, metricName string, number float64, labels map[string]string, defaultLabels *metric.FireflyDefaultLabels) {
-	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.ObserveHistogramMetricWithLabels(ctx, metricName, number, labels, defaultLabels)
+		mm.EvmHandlerMetricsManager.SetGaugeMetricWithLabels(ctx, metricName, number, labels, defaultLabels)
 	}
 }
 
-func (mm *metricsManager) ObserveTxHandlerSummaryMetric(ctx context.Context, metricName string, number float64, defaultLabels *metric.FireflyDefaultLabels) {
+func (mm *EvmMetricsManager) IncEvmHandlerCounterMetric(ctx context.Context, metricName string, defaultLabels *metric.FireflyDefaultLabels) {
 	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.ObserveSummaryMetric(ctx, metricName, number, defaultLabels)
+		mm.EvmHandlerMetricsManager.IncCounterMetric(ctx, metricName, defaultLabels)
 	}
 }
-func (mm *metricsManager) ObserveTxHandlerSummaryMetricWithLabels(ctx context.Context, metricName string, number float64, labels map[string]string, defaultLabels *metric.FireflyDefaultLabels) {
+func (mm *EvmMetricsManager) IncEvmHandlerCounterMetricWithLabels(ctx context.Context, metricName string, labels map[string]string, defaultLabels *metric.FireflyDefaultLabels) {
 	if mm.metricsEnabled {
-		mm.txHandlerMetricsManager.ObserveSummaryMetricWithLabels(ctx, metricName, number, labels, defaultLabels)
+		mm.EvmHandlerMetricsManager.IncCounterMetricWithLabels(ctx, metricName, labels, defaultLabels)
+	}
+}
+func (mm *EvmMetricsManager) ObserveEvmHandlerHistogramMetric(ctx context.Context, metricName string, number float64, defaultLabels *metric.FireflyDefaultLabels) {
+	if mm.metricsEnabled {
+		mm.EvmHandlerMetricsManager.ObserveHistogramMetric(ctx, metricName, number, defaultLabels)
+	}
+}
+func (mm *EvmMetricsManager) ObserveEvmHandlerHistogramMetricWithLabels(ctx context.Context, metricName string, number float64, labels map[string]string, defaultLabels *metric.FireflyDefaultLabels) {
+	if mm.metricsEnabled {
+		mm.EvmHandlerMetricsManager.ObserveHistogramMetricWithLabels(ctx, metricName, number, labels, defaultLabels)
+	}
+}
+
+func (mm *EvmMetricsManager) ObserveEvmHandlerSummaryMetric(ctx context.Context, metricName string, number float64, defaultLabels *metric.FireflyDefaultLabels) {
+	if mm.metricsEnabled {
+		mm.EvmHandlerMetricsManager.ObserveSummaryMetric(ctx, metricName, number, defaultLabels)
+	}
+}
+func (mm *EvmMetricsManager) ObserveEvmHandlerSummaryMetricWithLabels(ctx context.Context, metricName string, number float64, labels map[string]string, defaultLabels *metric.FireflyDefaultLabels) {
+	if mm.metricsEnabled {
+		mm.EvmHandlerMetricsManager.ObserveSummaryMetricWithLabels(ctx, metricName, number, labels, defaultLabels)
 	}
 }
