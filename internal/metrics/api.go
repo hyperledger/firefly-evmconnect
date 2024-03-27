c69668da4df799f58b89aff59fa88289ac263431
Philip-21 <philipuzomaobiora@gmail.com>
2024-03-27 22:50:43 +0100

    setting up evmconnect metrics manager
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- internal/metrics/api.go
+++ internal/metrics/api.go
@@ -7,34 +7,19 @@ import (
 )
 
 type Manager struct {
-	ctx               context.Context
-	metricsManager    EvmMetrics
-	metricsEnabled    bool
-	metricsHandled    bool
-	metricsServerDone chan error
-	fftm.Manager
+	ctx            context.Context
+	cancelCtx      func()
+	metricsManager EvmMetrics
+	fftm fftm.Manager
 }
 
-func (m *Manager) MetricsServer() {
-	if m.metricsEnabled {
-		go m.Start()
-	} else if m.metricsHandled {
-		go m.Close()
+func NewEvmMetricsManager(ctx context.Context, fftm fftm.Manager) error {
+	m := &Manager{
+		ctx:            ctx,
+		metricsManager: NewMetricsManager(ctx),
+		fftm: fftm,
+		
 	}
+	m.ctx, m.cancelCtx = context.WithCancel(ctx)
+	return nil
 }
-
-// func (m *Manager) CreateMetricsMuxRouter() *mux.Router {
-// 	r := mux.NewRouter()
-// 	r.Path(config.GetString(evmconfig.MetricsPath)).Handler(m.metricsManager.HTTPHandler())
-// 	return r
-// }
-
-// func (m *Manager) initServices(ctx context.Context) (err error) {
-// 	if m.metricsEnabled {
-// 		m.metricsServer, err = httpserver.NewHTTPServer(ctx, "metrics", m.CreateMetricsMuxRouter(), m.metricsServerDone, evmconfig.MetricsConfig, evmconfig.CorsConfig)
-// 		if err != nil {
-// 			return err
-// 		}
-// 	}
-// 	return nil
-// }
